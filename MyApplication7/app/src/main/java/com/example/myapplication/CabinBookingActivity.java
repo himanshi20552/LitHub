37053e48f9c5222ee798595473726ae3a7e34c4d
himanshi20552 <himanshi.chaturvedi.2005@gmail.com>
2025-05-05 01:41:27 +0530

    cabin form

--- MyApplication7/app/src/main/java/com/example/myapplication/CabinBookingActivity.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/CabinBookingActivity.java
@@ -2,9 +2,15 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -30,6 +36,10 @@ public class CabinBookingActivity extends AppCompatActivity {
         // Cabin booking buttons
         cabin1BookBtn.setOnClickListener(v -> bookCabin(1));
         cabin2BookBtn.setOnClickListener(v -> bookCabin(2));
+        cabin1BookBtn.setOnClickListener(v -> showBookingDialog());
+        cabin2BookBtn.setOnClickListener(v -> showBookingDialog());
+
+
 
         // Toggle between Cabin Booking and Virtual Groups
         toggleGroup.setOnCheckedChangeListener((group, checkedId) -> {
@@ -96,4 +106,38 @@ public class CabinBookingActivity extends AppCompatActivity {
             overridePendingTransition(0, 0);
         }
     }
+    public void showBookingDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.booking_form_dialog, null);
+        builder.setView(dialogView);
+
+        EditText en1 = dialogView.findViewById(R.id.en1);
+        EditText en2 = dialogView.findViewById(R.id.en2);
+        EditText en3 = dialogView.findViewById(R.id.en3);
+        EditText en4 = dialogView.findViewById(R.id.en4);
+        EditText ts = dialogView.findViewById(R.id.ts);
+
+        Button btnSubmit = dialogView.findViewById(R.id.submit_booking_button);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        btnSubmit.setOnClickListener(v -> {
+            String enr1 = en1.getText().toString().trim();
+            String enr2 = en2.getText().toString().trim();
+            String enr3 = en3.getText().toString().trim();
+            String enr4 = en4.getText().toString().trim();
+            String timeslot = ts.getText().toString().trim();
+
+
+            if (enr1.isEmpty() || enr2.isEmpty() || enr3.isEmpty()|| enr4.isEmpty()|| timeslot.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Cabin booked successfully!", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+    }
 }
+
